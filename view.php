4ad3620e796d4045f7ced1dc1517182ef448cb3e
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-07-27 04:14:19 +0530

    redirect the user to index if no pid

--- view.php
+++ view.php
@@ -2,4 +2,8 @@
 $cm=$db->prepare('SELECT pid, ptitle, pcont, pdate FROM posts WHERE pid = :pid');
 $cm->excute(array(':pid' => $_GET['id']));
 $row=$cm->fetch();
+if($row['pid']==''){
+	header('Location: ./');
+	exit;
+}
 ?>
