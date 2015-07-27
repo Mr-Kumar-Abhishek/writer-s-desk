e0e84495dd2d5dca6a001b45752e92c41ed181cd
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-07-27 11:34:49 +0530

    add backend for if login form submitted

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,14 @@
+<?php
+	require_once('../includes/config.php');
+	if(isset($_POST['submit'])) {
+		$uname = trim($_POST['uname']);
+		$passwd = trim($_POST['passwd']);
+		if($user->login($uname, $passwd)) {
+			header('Location: index.php');
+			exit;
+		}
+	}
+?>
 <from action="" method="post" >
 	<p><label>Username</label><input type="text" name="uname" value="" /></p>
 	<p><label>Password</label><input type="password" name="passwd" value="" /></p>
