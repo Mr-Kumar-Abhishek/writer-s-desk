c89c010492d1de5a920124c6bd5d7869ce345f31
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-07-27 11:26:10 +0530

    add login.php

---
+++ admin/login.php
@@ -0,0 +1,5 @@
+<from action="" method="post" >
+	<p><label>Username</label><input type="text" name="uname" value="" /></p>
+	<p><label>Password</label><input type="password" name="passwd" value="" /></p>
+	<p><lable></lable><input type="submit" name="submit" value="login" /></p>
+</from>
