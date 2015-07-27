bc9b2737cb7325f2c40a4c66f41dde41fb1b40a6
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-07-27 11:48:30 +0530

    checking if user signed in or not

--- classes/class.user.php
+++ classes/class.user.php
@@ -4,4 +4,8 @@
 	public function __construct($db) {
 		$this->db = $db;
 	}
+	//checking if the user is signed in or not
+	public function is_signed_in() {
+		if(isset($_SESSION['signedin']) && $_SESSION['signedin'] == true) return true;
+	}
 	
