e298fa4bc2c6b15e87f98c8aad43e4f630e3f044
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-10 20:06:45 +0530

    fix errors in database dump

--- db.sql
+++ db.sql
@@ -7,7 +7,7 @@ CREATE TABLE `blog_members` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`memberID`)
+  PRIMARY KEY (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `blog_members` WRITE;
@@ -22,11 +22,11 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `blog_posts`;
 
 CREATE TABLE `blog_posts` (
-  `postID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `postTitle` varchar(255) DEFAULT NULL,
   `postDesc` text,
   `postCont` text,
   `postDate` datetime DEFAULT NULL,
-  PRIMARY KEY (`postID`)
+  PRIMARY KEY (`post_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
