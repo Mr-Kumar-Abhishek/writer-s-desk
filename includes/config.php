bc2ebe260a28bb9379f4ad6fce6d4affe6855161
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-10 20:21:24 +0530

    define database connection

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+ob_start();
+session_start();
+
+//database credentials
+define('DBHOST','localhost'); // database name
+define('DBUSER','demo'); // database user
+define('DBPASS','demo'); // database password
+define('DBNAME','writer_s_desk'); // database name
+
+
