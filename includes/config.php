5c56a1c1544310da4672b948f7616930d06f3876
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-10 20:22:47 +0530

    open a PDO connection

--- includes/config.php
+++ includes/config.php
@@ -8,4 +8,6 @@ define('DBUSER','demo'); // database user
 define('DBPASS','demo'); // database password
 define('DBNAME','writer_s_desk'); // database name
 
+$db = new PDO("mysql:host=".DBHOST.";port=8889;dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
