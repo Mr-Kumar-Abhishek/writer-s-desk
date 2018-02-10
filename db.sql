143810f9f9e912b7425604f4741f444842d60e92
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-10 19:35:21 +0530

    add basic table structure

---
+++ db.sql
@@ -0,0 +1,32 @@
+
+
+DROP TABLE IF EXISTS `blog_members`;
+
+CREATE TABLE `blog_members` (
+  `member_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`memberID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `blog_members` WRITE;
+
+INSERT INTO `blog_members` (`member_id`, `username`, `password`, `email`)
+VALUES
+  (1,'Demo','$2y$10$wJxa1Wm0rtS2BzqKnoCPd.7QQzgu7D/aLlMR5Aw3O.m9jx3oRJ5R2','demo@demo.com');
+
+UNLOCK TABLES;
+
+
+DROP TABLE IF EXISTS `blog_posts`;
+
+CREATE TABLE `blog_posts` (
+  `postID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `postTitle` varchar(255) DEFAULT NULL,
+  `postDesc` text,
+  `postCont` text,
+  `postDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`postID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
