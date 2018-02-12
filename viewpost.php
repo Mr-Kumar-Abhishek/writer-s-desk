fc9b411a99b4cb8f88e9abd67a50a565f4750265
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-12 17:29:57 +0530

    if no post_id then redirect to index page

--- viewpost.php
+++ viewpost.php
@@ -2,4 +2,9 @@
 	$stmt = $db->prepare('SELECT post_id, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
 	$stmt->execute(array(':postID' => $_GET['id']));
 	$row = $stmt->fetch();
+	
+	if($row['post_id'] == ''){
+		header('Location: ./');
+		exit;
+	}
 ?>
