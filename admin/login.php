1dca13f0d0e2e47cf360534cd2e19fc9b3523794
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-12 17:57:12 +0530

    put in login process

--- admin/login.php
+++ admin/login.php
@@ -5,4 +5,32 @@
 	//if not logged in redirect to login page
 	if($user->is_logged_in()){ header('Location: index.php'); }
 
+	
+	//process login form if submitted
+	if(isset($_POST['submit'])){
+
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+    
+		if($user->login($username,$password)){ 
+
+			//logged in return to index page
+			header('Location: index.php');
+			exit;
+    
+
+		} else {
+			$message = '<p class="error">Wrong username or password</p>';
+		}
+
+	}//end if submit
+
+if(isset($message)){ echo $message; }
 ?>
+
+
+	<form action="" method="post">
+		<p><label>Username</label><input type="text" name="username" value=""  /></p>
+		<p><label>Password</label><input type="password" name="password" value=""  /></p>
+		<p><label></label><input type="submit" name="submit" value="Login"  /></p>
+	</form>
