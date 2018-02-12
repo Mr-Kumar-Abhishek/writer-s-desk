313e22093bf4353cd55cec9de9ba7615508638a7
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-12 18:05:04 +0530

    check if the session is logged in

--- classes/class.user.php
+++ classes/class.user.php
@@ -5,3 +5,10 @@
 	public function __construct($db){
 		$this->db = $db; 
 	}
+
+
+	public function is_logged_in(){
+		if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+			return true;
+		}        
+	}
