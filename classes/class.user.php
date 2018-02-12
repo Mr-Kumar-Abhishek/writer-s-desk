f50b0b0040793f2adc06a84cad7534235683417b
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-12 18:09:50 +0530

    get columns from the database

--- classes/class.user.php
+++ classes/class.user.php
@@ -12,3 +12,17 @@
 			return true;
 		}        
 	}
+
+	private function get_user_hash($username){
+
+		try {
+
+			$stmt = $this->_db->prepare('SELECT member_id, username, password FROM blog_members WHERE username = :username');
+			$stmt->execute(array('username' => $username));
+
+			return $stmt->fetch();
+
+		} catch(PDOException $e) {
+			echo '<p class="error">'.$e->getMessage().'</p>';
+		}
+	}
