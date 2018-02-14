33ecf4ec129868608bb9b8692a356e17a30b22de
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-14 17:08:52 +0530

    form for editing post

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -16,3 +16,22 @@
 		echo $e->getMessage();
 	}
 ?>
+
+
+<form action='' method='post'>
+    <input type='hidden' name='postID' value='<?php echo $row['postID'];?>'>
+
+    <p><label>Title</label><br />
+    <input type='text' name='postTitle' value='<?php echo $row['postTitle'];?>'></p>
+
+    <p><label>Description</label><br />
+    <textarea name='postDesc' cols='60' rows='10'><?php echo $row['postDesc'];?></textarea></p>
+
+    <p><label>Content</label><br />
+    <textarea name='postCont' cols='60' rows='10'><?php echo $row['postCont'];?></textarea></p>
+
+    <p><input type='submit' name='submit' value='Update'></p>
+
+</form>
+
+
