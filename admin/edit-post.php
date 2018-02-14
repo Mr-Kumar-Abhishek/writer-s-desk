54a029f59f64aff982d84074655b39f7283ed5be
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-14 17:11:52 +0530

    form validation for edit post form in php

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -15,6 +15,33 @@
 	} catch(PDOException $e) {
 		echo $e->getMessage();
 	}
+	
+	if(isset($_POST['submit'])){
+
+		$_POST = array_map( 'stripslashes', $_POST );
+
+		//collect form data
+		extract($_POST);
+
+		//very basic validation
+		if($postID ==''){
+			$error[] = 'This post is missing a valid id!.';
+		}
+
+		if($postTitle ==''){
+			$error[] = 'Please enter the title.';
+		}
+
+		if($postDesc ==''){
+			$error[] = 'Please enter the description.';
+		}
+
+		if($postCont ==''){
+			$error[] = 'Please enter the content.';
+		}
+	}
+	
+
 ?>
 
 
