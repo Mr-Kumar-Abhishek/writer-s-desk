b5575a0b96e8005b77d4dc36547103d339d627cc
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-14 17:27:02 +0530

    confirm to delete user

--- admin/users.php
+++ admin/users.php
@@ -29,3 +29,13 @@
 			}
 		?>
 </table>
+
+<script language="JavaScript" type="text/javascript">
+
+function deluser(id, title){
+	if (confirm("Are you sure you want to delete '" + title + "'")){
+      window.location.href = 'users.php?deluser=' + id;
+	}
+}
+
+</script>
