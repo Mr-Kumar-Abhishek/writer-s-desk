e2f8a00ff769ef19ab9b02db3085b249d97534a8
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-14 17:08:06 +0530

    get post data from database to edit post

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -4,4 +4,15 @@
 
 	//if not logged in redirect to login page
 	if(!$user->is_logged_in()){ header('Location: login.php'); }
+	
+	 
+	try {
+
+		$stmt = $db->prepare('SELECT postID, postTitle, postDesc, postCont FROM blog_posts WHERE postID = :postID');
+		$stmt->execute(array(':postID' => $_GET['id']));
+		$row = $stmt->fetch(); 
+
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
 ?>
