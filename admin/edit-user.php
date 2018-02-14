fa8db48dc736ed4fcc109f0b674c8a7ccc164f6a
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-14 17:38:17 +0530

    get user data to edit users

--- admin/edit-user.php
+++ admin/edit-user.php
@@ -4,4 +4,14 @@
 
 	//if not logged in redirect to login page
 	if(!$user->is_logged_in()){ header('Location: login.php'); }
+	
+	try {
+
+        $stmt = $db->prepare('SELECT memberID, username, email FROM blog_members WHERE memberID = :memberID') ;
+        $stmt->execute(array(':memberID' => $_GET['id']));
+        $row = $stmt->fetch(); 
+
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+    }
 ?>
