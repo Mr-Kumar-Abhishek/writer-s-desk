d76e72026db79a09c0803656e1d9263fcafde172
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 18:44:22 +0530

    update add-post.php

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,6 +1,8 @@
-<?php //include config
-require_once('../includes/config.php');
 
+<?php 
+
+//include config
+require_once('../includes/config.php');
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 ?>
@@ -11,89 +13,76 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
   <title>Admin - Add Post</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
   <link rel="stylesheet" href="../style/main.css">
+  <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
 </head>
 <body>
 
 <div id="wrapper">
 
-    <?php include('menu.php');?>
-    <p><a href="./">Blog Admin Index</a></p>
+	<?php include('menu.php');?>
+	<p><a href="./">Blog Admin Index</a></p>
 
-    <h2>Add Post</h2>
+	<h2>Add Post</h2>
 
-    <?php
+	<?php
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+		$_POST = array_map( 'stripslashes', $_POST );
+		//collect form data
+		extract($_POST);
+		//very basic validation
+		if($postTitle ==''){
+			$error[] = 'Please enter the title.';
+		}
+		if($postDesc ==''){
+			$error[] = 'Please enter the description.';
+		}
+		if($postCont ==''){
+			$error[] = 'Please enter the content.';
+		}
+		if(!isset($error)){
+			try {
+				//insert into database
+				$stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postCont,postDate) VALUES (:postTitle, :postDesc, :postCont, :postDate)') ;
+				$stmt->execute(array(
+					':postTitle' => $postTitle,
+					':postDesc' => $postDesc,
+					':postCont' => $postCont,
+					':postDate' => date('Y-m-d H:i:s')
+				));
+				//redirect to index page
+				header('Location: index.php?action=added');
+				exit;
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}
+		}
+	}
+	//check for any errors
+	if(isset($error)){
+		foreach($error as $error){
+			echo '<p class="error">'.$error.'</p>';
+		}
+	}
+	?>
 
-    //if form has been submitted process it
-    if(isset($_POST['submit'])){
+	<form action='' method='post'>
 
-        $_POST = array_map( 'stripslashes', $_POST );
+		<p><label>Title</label><br />
+		<input type='text' name='postTitle' value='<?php if(isset($error)){ echo $_POST['postTitle'];}?>'></p>
 
-        //collect form data
-        extract($_POST);
+		<p><label>Description</label><br />
+		<textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
 
-        //very basic validation
-        if($postTitle ==''){
-            $error[] = 'Please enter the title.';
-        }
+		<p><label>Content</label><br />
+		<textarea name='postCont' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postCont'];}?></textarea></p>
 
-        if($postDesc ==''){
-            $error[] = 'Please enter the description.';
-        }
+		<p><input type='submit' name='submit' value='Submit'></p>
 
-        if($postCont ==''){
-            $error[] = 'Please enter the content.';
-        }
+	</form>
 
-        if(!isset($error)){
-
-            try {
-
-                //insert into database
-                $stmt = $db->prepare('INSERT INTO blog_posts (postTitle,postDesc,postCont,postDate) VALUES (:postTitle, :postDesc, :postCont, :postDate)') ;
-                $stmt->execute(array(
-                    ':postTitle' => $postTitle,
-                    ':postDesc' => $postDesc,
-                    ':postCont' => $postCont,
-                    ':postDate' => date('Y-m-d H:i:s')
-                ));
-
-                //redirect to index page
-                header('Location: index.php?action=added');
-                exit;
-
-            } catch(PDOException $e) {
-                echo $e->getMessage();
-            }
-
-        }
-
-    }
-
-    //check for any errors
-    if(isset($error)){
-        foreach($error as $error){
-            echo '<p class="error">'.$error.'</p>';
-        }
-    }
-    ?>
-
-    <form action='' method='post'>
-
-        <p><label>Title</label><br />
-        <input type='text' name='postTitle' value='<?php if(isset($error)){ echo $_POST['postTitle'];}?>'></p>
-
-        <p><label>Description</label><br />
-        <textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
-
-        <p><label>Content</label><br />
-        <textarea name='postCont' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postCont'];}?></textarea></p>
-
-        <p><input type='submit' name='submit' value='Submit'></p>
-
-    </form>
 </div>
-      <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
-	  <script>
+  <script>
           tinymce.init({
               selector: "textarea",
               plugins: [
@@ -103,6 +92,6 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
               ],
               toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
           });
-	  </script>
+  </script>
 </body>
 </html>
