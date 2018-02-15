c89ff2516e28d8f2468b7218c53c716e561ad990
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 16:33:46 +0530

    autoload classes if call within admin adjust path

--- includes/config.php
+++ includes/config.php
@@ -20,17 +20,23 @@ function __autoload($class) {
    
    $class = strtolower($class);
 
+    //if call from within /assets adjust the path
    $classpath = 'classes/class.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
     }     
-
+	
+   //if call from within admin adjust the path
    $classpath = '../classes/class.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
     }
-        
-     
+    
+   //if call from within admin adjust the path
+   $classpath = '../../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+    }          
 }
 
 $user = new User($db); 
