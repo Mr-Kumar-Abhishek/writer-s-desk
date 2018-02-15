bda02af86e60a5abd23cc1b248f7f947cca02665
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 18:47:46 +0530

    update viewpost.php

--- viewpost.php
+++ viewpost.php
@@ -1,49 +1,39 @@
-<?php
-
-	require('includes/config.php'); 
-	
-	$stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
-	$stmt->execute(array(':postID' => $_GET['id']));
-	$row = $stmt->fetch();
-	
-	
-	// if post does not exist redirect user.
-	if($row['postID'] == ''){
-		header('Location: ./');
-		exit;
-	}
-
+<?php require('includes/config.php'); 
+$stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
+$stmt->execute(array(':postID' => $_GET['id']));
+$row = $stmt->fetch();
+//if post does not exists redirect user.
+if($row['postID'] == ''){
+	header('Location: ./');
+	exit;
+}
 ?>
-
-
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Blog - <?php echo $row['postTitle'];?></title>
-    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
     <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
 
-    <div id="wrapper">
+	<div id="wrapper">
 
-        <h1>Blog</h1>
-        <hr />
-        <p><a href="./">Blog Index</a></p>
+		<h1>Blog</h1>
+		<hr />
+		<p><a href="./">Blog Index</a></p>
 
-		<?php
+
+		<?php	
 			echo '<div>';
-			echo '<h1>'.$row['postTitle'].'</h1>';
-			echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
-			echo '<p>'.$row['postCont'].'</p>';                
+				echo '<h1>'.$row['postTitle'].'</h1>';
+				echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
+				echo '<p>'.$row['postCont'].'</p>';				
 			echo '</div>';
-
 		?>
 
-    </div>
-
+	</div>
 
 </body>
 </html>
