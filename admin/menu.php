98dd1c6fd65e28e0e0263d0dd09dcf61468f9759
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 16:57:45 +0530

    update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -1,13 +1,4 @@
-<?php
-	//include config
-	require_once('../includes/config.php');
-
-	//if not logged in redirect to login page
-	if(!$user->is_logged_in()){ header('Location: login.php'); }
-?>
-
 <h1>Blog</h1>
-<p>Logged in as <?=$_SESSION['username'];?></p>
 <ul id='adminmenu'>
     <li><a href='index.php'>Blog</a></li>
     <li><a href='users.php'>Users</a></li>
