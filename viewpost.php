fd83b76e7b0f375e347432e1cd6186cb5d049488
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 16:27:03 +0530

    put in required html in viewpost.php

--- viewpost.php
+++ viewpost.php
@@ -6,15 +6,44 @@
 	$stmt->execute(array(':postID' => $_GET['id']));
 	$row = $stmt->fetch();
 	
+	
+	// if post does not exist redirect user.
 	if($row['post_id'] == ''){
 		header('Location: ./');
 		exit;
 	}
-	
-	echo '<div>';
-    echo '<h1>'.$row['postTitle'].'</h1>';
-    echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
-    echo '<p>'.$row['postCont'].'</p>';                
-	echo '</div>';
 
 ?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Blog - <?php echo $row['postTitle'];?></title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
+
+    <div id="wrapper">
+
+        <h1>Blog</h1>
+        <hr />
+        <p><a href="./">Blog Index</a></p>
+
+		<?php
+			echo '<div>';
+			echo '<h1>'.$row['postTitle'].'</h1>';
+			echo '<p>Posted on '.date('jS M Y', strtotime($row['postDate'])).'</p>';
+			echo '<p>'.$row['postCont'].'</p>';                
+			echo '</div>';
+
+		?>
+
+    </div>
+
+
+</body>
+</html>
