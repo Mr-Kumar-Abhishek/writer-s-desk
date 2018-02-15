2b5bbbd7cf6f335d5e55042a924a091a6c7f1d26
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 17:17:48 +0530

    update users.php

--- admin/users.php
+++ admin/users.php
@@ -1,11 +1,12 @@
 <?php
-	//include config
-	require_once('../includes/config.php');
+//include config
+require_once('../includes/config.php');
 
-	//if not logged in redirect to login page
-	if(!$user->is_logged_in()){ header('Location: login.php'); }
-	
-	if(isset($_GET['deluser'])){ 
+//if not logged in redirect to login page
+if(!$user->is_logged_in()){ header('Location: login.php'); }
+
+//show message from add / edit page
+if(isset($_GET['deluser'])){ 
 
     //if user id is 1 ignore
     if($_GET['deluser'] !='1'){
@@ -18,38 +19,76 @@
 
     }
 } 
+
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Admin - Users</title>
+  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="../style/main.css">
+  <script language="JavaScript" type="text/javascript">
+  function deluser(id, title)
+  {
+      if (confirm("Are you sure you want to delete '" + title + "'"))
+      {
+          window.location.href = 'users.php?deluser=' + id;
+      }
+  }
+  </script>
+</head>
+<body>
 
-<table>
-	<?php
-		$stmt = $db->query('SELECT memberID, username, email FROM blog_members ORDER BY username');
-		while($row = $stmt->fetch()){
-    
-			echo '<tr>';
-			echo '<td>'.$row['username'].'</td>';
-			echo '<td>'.$row['email'].'</td>';
-	?>
+    <div id="wrapper">
 
-    <td>
-        <a href="edit-user.php?id=<?php echo $row['memberID'];?>">Edit</a> 
-        <?php if($row['memberID'] != 1){?>
-            | <a href="javascript:deluser('<?php echo $row['memberID'];?>','<?php echo $row['username'];?>')">Delete</a>
-        <?php } ?>
-    </td>
-    
-		<?php 
-			echo '</tr>';
+    <?php include('menu.php');?>
 
-			}
-		?>
-</table>
+    <?php 
+    //show message from add / edit page
+    if(isset($_GET['action'])){ 
+        echo '<h3>User '.$_GET['action'].'.</h3>'; 
+    } 
+    ?>
 
-<script language="JavaScript" type="text/javascript">
+    <table>
+    <tr>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Action</th>
+    </tr>
+    <?php
+        try {
 
-function deluser(id, title){
-	if (confirm("Are you sure you want to delete '" + title + "'")){
-      window.location.href = 'users.php?deluser=' + id;
-	}
-}
+            $stmt = $db->query('SELECT memberID, username, email FROM blog_members ORDER BY username');
+            while($row = $stmt->fetch()){
+                
+                echo '<tr>';
+                echo '<td>'.$row['username'].'</td>';
+                echo '<td>'.$row['email'].'</td>';
+                ?>
 
-</script>
+                <td>
+                    <a href="edit-user.php?id=<?php echo $row['memberID'];?>">Edit</a> 
+                    <?php if($row['memberID'] != 1){?>
+                        | <a href="javascript:deluser('<?php echo $row['memberID'];?>','<?php echo $row['username'];?>')">Delete</a>
+                    <?php } ?>
+                </td>
+                
+                <?php 
+                echo '</tr>';
+
+            }
+
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+        }
+    ?>
+    </table>
+
+    <p><a href='add-user.php'>Add User</a></p>
+
+</div>
+
+</body>
+</html>
