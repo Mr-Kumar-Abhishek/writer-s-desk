c430f32c17aaba3c7767ba419deeb1df81b32e6c
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 18:22:05 +0530

    fix bug of not adding new users

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,6 +1,5 @@
 <?php //include config
 require_once('../includes/config.php');
-
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 ?>
@@ -16,91 +15,75 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 
 <div id="wrapper">
 
-    <?php include('menu.php');?>
-    <p><a href="users.php">User Admin Index</a></p>
+	<?php include('menu.php');?>
+	<p><a href="users.php">User Admin Index</a></p>
 
-    <h2>Add User</h2>
+	<h2>Add User</h2>
 
-    <?php
+	<?php
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+		//collect form data
+		extract($_POST);
+		//very basic validation
+		if($username ==''){
+			$error[] = 'Please enter the username.';
+		}
+		if($password ==''){
+			$error[] = 'Please enter the password.';
+		}
+		if($passwordConfirm ==''){
+			$error[] = 'Please confirm the password.';
+		}
+		if($password != $passwordConfirm){
+			$error[] = 'Passwords do not match.';
+		}
+		if($email ==''){
+			$error[] = 'Please enter the email address.';
+		}
+		if(!isset($error)){
+			$hashedpassword = $user->password_hash($password, PASSWORD_BCRYPT);
+			try {
+				//insert into database
+				$stmt = $db->prepare('INSERT INTO blog_members (username,password,email) VALUES (:username, :password, :email)') ;
+				$stmt->execute(array(
+					':username' => $username,
+					':password' => $hashedpassword,
+					':email' => $email
+				));
+				//redirect to index page
+				header('Location: users.php?action=added');
+				exit;
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}
+		}
+	}
+	//check for any errors
+	if(isset($error)){
+		foreach($error as $error){
+			echo '<p class="error">'.$error.'</p>';
+		}
+	}
+	?>
 
-    //if form has been submitted process it
-    if(isset($_POST['submit'])){
+	<form action='' method='post'>
 
-        //collect form data
-        extract($_POST);
+		<p><label>Username</label><br />
+		<input type='text' name='username' value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
 
-        //very basic validation
-        if($username ==''){
-            $error[] = 'Please enter the username.';
-        }
+		<p><label>Password</label><br />
+		<input type='password' name='password' value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
 
-        if($password ==''){
-            $error[] = 'Please enter the password.';
-        }
+		<p><label>Confirm Password</label><br />
+		<input type='password' name='passwordConfirm' value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
 
-        if($passwordConfirm ==''){
-            $error[] = 'Please confirm the password.';
-        }
+		<p><label>Email</label><br />
+		<input type='text' name='email' value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
+		
+		<p><input type='submit' name='submit' value='Add User'></p>
 
-        if($password != $passwordConfirm){
-            $error[] = 'Passwords do not match.';
-        }
-
-        if($email ==''){
-            $error[] = 'Please enter the email address.';
-        }
-
-        if(!isset($error)){
-
-            $hashedpassword = $user->create_hash($password);
-
-            try {
-
-                //insert into database
-                $stmt = $db->prepare('INSERT INTO blog_members (username,password,email) VALUES (:username, :password, :email)') ;
-                $stmt->execute(array(
-                    ':username' => $username,
-                    ':password' => $hashedpassword,
-                    ':email' => $email
-                ));
-
-                //redirect to index page
-                header('Location: users.php?action=added');
-                exit;
-
-            } catch(PDOException $e) {
-                echo $e->getMessage();
-            }
-
-        }
-
-    }
-
-    //check for any errors
-    if(isset($error)){
-        foreach($error as $error){
-            echo '<p class="error">'.$error.'</p>';
-        }
-    }
-    ?>
-
-    <form action='' method='post'>
-
-        <p><label>Username</label><br />
-        <input type='text' name='username' value='<?php if(isset($error)){ echo $_POST['username'];}?>'></p>
-
-        <p><label>Password</label><br />
-        <input type='password' name='password' value='<?php if(isset($error)){ echo $_POST['password'];}?>'></p>
-
-        <p><label>Confirm Password</label><br />
-        <input type='password' name='passwordConfirm' value='<?php if(isset($error)){ echo $_POST['passwordConfirm'];}?>'></p>
-
-        <p><label>Email</label><br />
-        <input type='text' name='email' value='<?php if(isset($error)){ echo $_POST['email'];}?>'></p>
-        
-        <p><input type='submit' name='submit' value='Add User'></p>
-
-    </form>
+	</form>
 
 </div>
 </body>
