ab5cbd42a61521e5d9e8e0ac2a70df0262ad475b
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 17:23:04 +0530

    use CDN for normalize.css in add-user.php

--- admin/add-user.php
+++ admin/add-user.php
@@ -9,7 +9,7 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 <head>
   <meta charset="utf-8">
   <title>Admin - Add User</title>
-  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
   <link rel="stylesheet" href="../style/main.css">
 </head>
 <body>
