2e67ac3f5a0b1b6f88b55ee5b5a512b5b86a4972
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-15 16:15:33 +0530

    require configs in viewposts.php

--- viewpost.php
+++ viewpost.php
@@ -1,6 +1,7 @@
-
-
 <?php
+
+	require('includes/config.php'); 
+	
 	$stmt = $db->prepare('SELECT postID, postTitle, postCont, postDate FROM blog_posts WHERE postID = :postID');
 	$stmt->execute(array(':postID' => $_GET['id']));
 	$row = $stmt->fetch();
