004e87da0826025a474af0fcc4a1a597a0b253e8
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-17 18:14:32 +0530

    update config.php for seo urls

--- includes/config.php
+++ includes/config.php
@@ -14,29 +14,29 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //set timezone
 date_default_timezone_set('Asia/Kolkata');
 
-
 //load classes as needed
 function __autoload($class) {
    
    $class = strtolower($class);
-
-    //if call from within /assets adjust the path
+	//if call from within assets adjust the path
    $classpath = 'classes/class.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
-    }     
+	} 	
 	
-   //if call from within admin adjust the path
+	//if call from within admin adjust the path
    $classpath = '../classes/class.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
-    }
-    
-   //if call from within admin adjust the path
+	}
+	
+	//if call from within admin adjust the path
    $classpath = '../../classes/class.'.$class . '.php';
    if ( file_exists($classpath)) {
       require_once $classpath;
-    }          
+	} 		
+	 
 }
-
 $user = new User($db); 
+include('functions.php');
+?>
