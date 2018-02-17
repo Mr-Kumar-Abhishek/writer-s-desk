33d5e853db49951601e03abdd220fcaaf97d0ce7
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-17 16:45:54 +0530

    update admin/index.php

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,15 @@
 <?php
 //include config
 require_once('../includes/config.php');
-
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
-
 //show message from add / edit page
 if(isset($_GET['delpost'])){ 
-
-    $stmt = $db->prepare('DELETE FROM blog_posts WHERE postID = :postID') ;
-    $stmt->execute(array(':postID' => $_GET['delpost']));
-
-    header('Location: index.php?action=deleted');
-    exit;
+	$stmt = $db->prepare('DELETE FROM blog_posts WHERE postID = :postID') ;
+	$stmt->execute(array(':postID' => $_GET['delpost']));
+	header('Location: index.php?action=deleted');
+	exit;
 } 
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,65 +18,61 @@ if(isset($_GET['delpost'])){
   <title>Admin</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
   <link rel="stylesheet" href="../style/main.css">
-  <script language="JavaScript" type="text/javascript">
-  function delpost(id, title)
-  {
-      if (confirm("Are you sure you want to delete '" + title + "'"))
-      {
-          window.location.href = 'index.php?delpost=' + id;
-      }
-  }
-  </script>
 </head>
 <body>
 
-    <div id="wrapper">
+	<div id="wrapper">
 
-    <?php include('menu.php');?>
+	<?php include('menu.php');?>
 
-    <?php 
-    //show message from add / edit page
-    if(isset($_GET['action'])){ 
-        echo '<h3>Post '.$_GET['action'].'.</h3>'; 
-    } 
-    ?>
+	<?php 
+	//show message from add / edit page
+	if(isset($_GET['action'])){ 
+		echo '<h3>Post '.$_GET['action'].'.</h3>'; 
+	} 
+	?>
 
-    <table>
-    <tr>
-        <th>Title</th>
-        <th>Date</th>
-        <th>Action</th>
-    </tr>
-    <?php
-        try {
+	<table>
+	<tr>
+		<th>Title</th>
+		<th>Date</th>
+		<th>Action</th>
+	</tr>
+	<?php
+		try {
+			$stmt = $db->query('SELECT postID, postTitle, postDate FROM blog_posts ORDER BY postID DESC');
+			while($row = $stmt->fetch()){
+				
+				echo '<tr>';
+				echo '<td>'.$row['postTitle'].'</td>';
+				echo '<td>'.date('jS M Y', strtotime($row['postDate'])).'</td>';
+				?>
 
-            $stmt = $db->query('SELECT postID, postTitle, postDate FROM blog_posts ORDER BY postID DESC');
-            while($row = $stmt->fetch()){
-                
-                echo '<tr>';
-                echo '<td>'.$row['postTitle'].'</td>';
-                echo '<td>'.date('jS M Y', strtotime($row['postDate'])).'</td>';
-                ?>
+				<td>
+					<a href="edit-post.php?id=<?php echo $row['postID'];?>">Edit</a> | 
+					<a href="javascript:delpost('<?php echo $row['postID'];?>','<?php echo $row['postTitle'];?>')">Delete</a>
+				</td>
+				
+				<?php 
+				echo '</tr>';
+			}
+		} catch(PDOException $e) {
+		    echo $e->getMessage();
+		}
+	?>
+	</table>
 
-                <td>
-                    <a href="edit-post.php?id=<?php echo $row['postID'];?>">Edit</a> | 
-                    <a href="javascript:delpost('<?php echo $row['postID'];?>','<?php echo $row['postTitle'];?>')">Delete</a>
-                </td>
-                
-                <?php 
-                echo '</tr>';
-
-            }
-
-        } catch(PDOException $e) {
-            echo $e->getMessage();
-        }
-    ?>
-    </table>
-
-    <p><a href='add-post.php'>Add Post</a></p>
+	<p><a href='add-post.php'>Add Post</a></p>
 
 </div>
-
+  <script language="JavaScript" type="text/javascript">
+  function delpost(id, title)
+  {
+	  if (confirm("Are you sure you want to delete '" + title + "'"))
+	  {
+	  	window.location.href = 'index.php?delpost=' + id;
+	  }
+  }
+  </script>
 </body>
 </html>
