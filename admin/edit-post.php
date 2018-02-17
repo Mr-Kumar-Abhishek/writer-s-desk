4e987b8b1cb1f921fccf31240c8ba382d013d1bc
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-17 16:41:34 +0530

    update edit-post.php

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -1,6 +1,5 @@
 <?php //include config
 require_once('../includes/config.php');
-
 //if not logged in redirect to login page
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 ?>
@@ -16,102 +15,83 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 
 <div id="wrapper">
 
-    <?php include('menu.php');?>
-    <p><a href="./">Blog Admin Index</a></p>
+	<?php include('menu.php');?>
+	<p><a href="./">Blog Admin Index</a></p>
 
-    <h2>Edit Post</h2>
-
-
-    <?php
-
-    //if form has been submitted process it
-    if(isset($_POST['submit'])){
-
-        $_POST = array_map( 'stripslashes', $_POST );
-
-        //collect form data
-        extract($_POST);
-
-        //very basic validation
-        if($postID ==''){
-            $error[] = 'This post is missing a valid id!.';
-        }
-
-        if($postTitle ==''){
-            $error[] = 'Please enter the title.';
-        }
-
-        if($postDesc ==''){
-            $error[] = 'Please enter the description.';
-        }
-
-        if($postCont ==''){
-            $error[] = 'Please enter the content.';
-        }
-
-        if(!isset($error)){
-
-            try {
-
-                //insert into database
-                $stmt = $db->prepare('UPDATE blog_posts SET postTitle = :postTitle, postDesc = :postDesc, postCont = :postCont WHERE postID = :postID') ;
-                $stmt->execute(array(
-                    ':postTitle' => $postTitle,
-                    ':postDesc' => $postDesc,
-                    ':postCont' => $postCont,
-                    ':postID' => $postID
-                ));
-
-                //redirect to index page
-                header('Location: index.php?action=updated');
-                exit;
-
-            } catch(PDOException $e) {
-                echo $e->getMessage();
-            }
-
-        }
-
-    }
-
-    ?>
+	<h2>Edit Post</h2>
 
 
-    <?php
-    //check for any errors
-    if(isset($error)){
-        foreach($error as $error){
-            echo $error.'<br />';
-        }
-    }
+	<?php
+	//if form has been submitted process it
+	if(isset($_POST['submit'])){
+		$_POST = array_map( 'stripslashes', $_POST );
+		//collect form data
+		extract($_POST);
+		//very basic validation
+		if($postID ==''){
+			$error[] = 'This post is missing a valid id!.';
+		}
+		if($postTitle ==''){
+			$error[] = 'Please enter the title.';
+		}
+		if($postDesc ==''){
+			$error[] = 'Please enter the description.';
+		}
+		if($postCont ==''){
+			$error[] = 'Please enter the content.';
+		}
+		if(!isset($error)){
+			try {
+				//insert into database
+				$stmt = $db->prepare('UPDATE blog_posts SET postTitle = :postTitle, postDesc = :postDesc, postCont = :postCont WHERE postID = :postID') ;
+				$stmt->execute(array(
+					':postTitle' => $postTitle,
+					':postDesc' => $postDesc,
+					':postCont' => $postCont,
+					':postID' => $postID
+				));
+				//redirect to index page
+				header('Location: index.php?action=updated');
+				exit;
+			} catch(PDOException $e) {
+			    echo $e->getMessage();
+			}
+		}
+	}
+	?>
 
-        try {
 
-            $stmt = $db->prepare('SELECT postID, postTitle, postDesc, postCont FROM blog_posts WHERE postID = :postID') ;
-            $stmt->execute(array(':postID' => $_GET['id']));
-            $row = $stmt->fetch(); 
+	<?php
+	//check for any errors
+	if(isset($error)){
+		foreach($error as $error){
+			echo $error.'<br />';
+		}
+	}
+		try {
+			$stmt = $db->prepare('SELECT postID, postTitle, postDesc, postCont FROM blog_posts WHERE postID = :postID') ;
+			$stmt->execute(array(':postID' => $_GET['id']));
+			$row = $stmt->fetch(); 
+		} catch(PDOException $e) {
+		    echo $e->getMessage();
+		}
+	?>
 
-        } catch(PDOException $e) {
-            echo $e->getMessage();
-        }
+	<form action='' method='post'>
+		<input type='hidden' name='postID' value='<?php echo $row['postID'];?>'>
 
-    ?>
+		<p><label>Title</label><br />
+		<input type='text' name='postTitle' value='<?php echo $row['postTitle'];?>'></p>
 
-    <form action='' method='post'>
-        <input type='hidden' name='postID' value='<?php echo $row['postID'];?>'>
+		<p><label>Description</label><br />
+		<textarea name='postDesc' cols='60' rows='10'><?php echo $row['postDesc'];?></textarea></p>
 
-        <p><label>Title</label><br />
-        <input type='text' name='postTitle' value='<?php echo $row['postTitle'];?>'></p>
+		<p><label>Content</label><br />
+		<textarea name='postCont' cols='60' rows='10'><?php echo $row['postCont'];?></textarea></p>
 
-        <p><label>Description</label><br />
-        <textarea name='postDesc' cols='60' rows='10'><?php echo $row['postDesc'];?></textarea></p>
+		<p><input type='submit' name='submit' value='Update'></p>
 
-        <p><label>Content</label><br />
-        <textarea name='postCont' cols='60' rows='10'><?php echo $row['postCont'];?></textarea></p>
-
-        <p><input type='submit' name='submit' value='Update'></p>
-
-    </form>
+	</form>
 
 </div>
   <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script>
@@ -127,4 +107,4 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
           });
   </script>
 </body>
-</html> 
+</html>	
