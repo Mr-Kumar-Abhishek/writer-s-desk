d4c279ae8b941b4294830b8da4e9acf327ce7019
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-17 16:48:41 +0530

    update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,6 @@
 <?php
 //include config
 require_once('../includes/config.php');
-
-
 //check if already logged in
 if( $user->is_logged_in() ){ header('Location: index.php'); } 
 ?>
@@ -11,42 +9,36 @@ if( $user->is_logged_in() ){ header('Location: index.php'); }
 <head>
   <meta charset="utf-8">
   <title>Admin Login</title>
-  <link rel="stylesheet" href="../style/normalize.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
   <link rel="stylesheet" href="../style/main.css">
 </head>
 <body>
 
 <div id="login">
 
-    <?php
+	<?php
+	//process login form if submitted
+	if(isset($_POST['submit'])){
+		$username = trim($_POST['username']);
+		$password = trim($_POST['password']);
+		
+		if($user->login($username,$password)){ 
+			//logged in return to index page
+			header('Location: index.php');
+			exit;
+		
+		} else {
+			$message = '<p class="error">Wrong username or password</p>';
+		}
+	}//end if submit
+	if(isset($message)){ echo $message; }
+	?>
 
-    //process login form if submitted
-    if(isset($_POST['submit'])){
-
-        $username = trim($_POST['username']);
-        $password = trim($_POST['password']);
-        
-        if($user->login($username,$password)){ 
-
-            //logged in return to index page
-            header('Location: index.php');
-            exit;
-        
-
-        } else {
-            $message = '<p class="error">Wrong username or password</p>';
-        }
-
-    }//end if submit
-
-    if(isset($message)){ echo $message; }
-    ?>
-
-    <form action="" method="post">
-    <p><label>Username</label><input type="text" name="username" value=""  /></p>
-    <p><label>Password</label><input type="password" name="password" value=""  /></p>
-    <p><label></label><input type="submit" name="submit" value="Login"  /></p>
-    </form>
+	<form action="" method="post">
+	<p><label>Username</label><input type="text" name="username" value=""  /></p>
+	<p><label>Password</label><input type="password" name="password" value=""  /></p>
+	<p><label></label><input type="submit" name="submit" value="Login"  /></p>
+	</form>
 
 </div>
 </body>
