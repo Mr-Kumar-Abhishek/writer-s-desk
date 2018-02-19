b045b8518168cf0f06b78f024926e2263cddfc98
Abhishek Kumar <mr.kumar.abhishek@email.com>
2018-02-19 17:32:34 +0530

    update edit-post.php for seo urls

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -42,10 +42,12 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 		}
 		if(!isset($error)){
 			try {
+				$postSlug = slug($postTitle);
 				//insert into database
-				$stmt = $db->prepare('UPDATE blog_posts SET postTitle = :postTitle, postDesc = :postDesc, postCont = :postCont WHERE postID = :postID') ;
+				$stmt = $db->prepare('UPDATE blog_posts_seo SET postTitle = :postTitle, postSlug = :postSlug, postDesc = :postDesc, postCont = :postCont WHERE postID = :postID') ;
 				$stmt->execute(array(
 					':postTitle' => $postTitle,
+					':postSlug' => $postSlug,
 					':postDesc' => $postDesc,
 					':postCont' => $postCont,
 					':postID' => $postID
@@ -69,7 +71,7 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 		}
 	}
 		try {
-			$stmt = $db->prepare('SELECT postID, postTitle, postDesc, postCont FROM blog_posts WHERE postID = :postID') ;
+			$stmt = $db->prepare('SELECT postID, postTitle, postDesc, postCont FROM blog_posts_seo WHERE postID = :postID') ;
 			$stmt->execute(array(':postID' => $_GET['id']));
 			$row = $stmt->fetch(); 
 		} catch(PDOException $e) {
